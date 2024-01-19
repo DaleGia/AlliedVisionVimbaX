c40309c8f9fddd728ee6647e1de8fe4e2a1823b4
dfn <d.giancono@gmail.com>
2024-01-19 17:39:20 +0800

    Adding install script that allows user to define arm or amd

--- install.sh
+++ install.sh
@@ -1,10 +1,27 @@
 # Delete old installation
 sudo rm /usr/local/lib/VimbaX
-# If you need ARM, change to VimbaX_Setup-2023-1-Linux64_ARM.tar.gz
-tar -xzvf VimbaX_Setup-2023-1-Linux64.tar.gz
+
+# If you need ARM, change to VimbaX_Setup-2023-1-Linux_ARM64.tar.gz
+# tar -xzvf VimbaX_Setup-2023-1-Linux64.tar.gz
+# tar -xzvf VimbaX_Setup-2023-1-Linux_ARM64.tar.gz
+while true; do
+    echo "Install amd64 (1) or ARM64 (2)?"
+    read -n1 input
+    if [ "$input" = "1" ]; then
+        tar -xzvf VimbaX_Setup-2023-1-Linux_ARM64.tar.gz
+        break
+    elif [ "$input" = "2" ]; then
+        tar -xzvf VimbaX_Setup-2023-1-Linux_ARM64.tar.gz
+        break
+    else
+        echo "Try again"
+    fi
+done
+
 cd VimbaX_2023-1/cti/
 sudo sh VimbaUSBTL_Install.sh
 cd ../../
 sudo cp -r VimbaX_2023-1 /usr/local/lib/VimbaX
+rm -r VimbaX_2023-1/
 sudo ldconfig
 
